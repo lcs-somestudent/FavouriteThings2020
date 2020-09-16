8fd617060e6bc2c50523700c62e9f119e49e8cc2
Russell Gordon <rgordon@lcs.on.ca>
2020-09-15 21:52:24 -0400

    Initial Commit

---
+++ FavouriteThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouriteThings
+//
+//  Created by Russell Gordon on 2020-09-15.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
