4a661dbe65abb354d0d8965be2231e5c38711809
Russell Gordon <github@russellgordon.ca>
2020-09-16 09:05:21 -0400

    Making progress on my "About me" page.

--- FavouriteThings/ContentView.swift
+++ FavouriteThings/ContentView.swift
@@ -15,27 +15,41 @@ struct ContentView: View {
         
         ScrollView {
             
-            VStack {
+            VStack(spacing: 10.0) {
                 
-                HStack {
-                    
-                    Image("Me")
-                        .resizable()
-                        .scaledToFit()
-                    
-                    Text("Hello World!")
-                    
-                    
-                }
-                .padding(.horizontal)
+                Text("A bit about me")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
                 
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit aliquet sagittis id consectetur. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Quam nulla porttitor massa id neque aliquam vestibulum. Amet purus gravida quis blandit turpis cursus in hac habitasse. Sagittis nisl rhoncus mattis rhoncus urna neque viverra. At volutpat diam ut venenatis tellus. Magnis dis parturient montes nascetur ridiculus mus. Sollicitudin aliquam ultrices sagittis orci a. Massa ultricies mi quis hendrerit dolor magna eget. Fermentum iaculis eu non diam phasellus vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit aliquet sagittis id consectetur. Aliquam ultrices sagittis orci a scelerisque purus semper eget duis. Quam nulla porttitor massa id neque aliquam vestibulum. Amet purus gravida quis blandit turpis cursus in hac habitasse. Sagittis nisl rhoncus mattis rhoncus urna neque viverra. At volutpat diam ut venenatis tellus. Magnis dis parturient montes nascetur ridiculus mus. Sollicitudin aliquam ultrices sagittis orci a. Massa ultricies mi quis hendrerit dolor magna eget. Fermentum iaculis eu non diam phasellus vestibulum. ")
-                    .font(.body)
-                    .foregroundColor(Color.red)
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal)
+                Image("Me")
+                    .resizable()
+                    .scaledToFit()
+
+                Text("I grew up in Kitchener, Ontario, Canada. This is a medium-sized city in the southwestern corner of the province, about an hour's drive away from Toronto.")
+                
+                Image("Kitchener")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("My favourite thing to do with my family when growing up was watching baseball. We'd watch on TV, and usually go to at least one game a year in Toronto. A long time ago, the Blue Jays played at Exhibition Stadium. It was a stadium not really designed for baseball, right by the waterfront on Lake Ontario.")
+                
+                Image("exhibition-stadium")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("The odd thing about it is that depending on whether you sat in the grandstand (covered) or the other seats (no cover) the temperature could vary widely. It was very common to see some fans in heavy jackets on one side of the field, and in shorts and t-shirts and on the other side of the field! 😎")
+                
+                Text("My favourite baseball player growing up was Dave Stieb. Dave Stieb was a pitcher. I liked Stieb in part because you never had to guess how he was feeling. If he was happy, you'd see that. If he was... not happy, well, you'd see that too. I like an athlete who shows a little emotion when they play.")
+                
+                Image("dave-stieb")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("In this photo, he's in the back, being mobbed by his teammates after pitching a no-hitter against the Cleveland Indians in Cleveland on September 2, 1990. That's a very special event for a pitcher in baseball – none of the opposing players, for nine innings, got a hit. These days, it's rare for a pitcher to even complete a game, let alone a no-hitter. Very cool. ⚾️")
+
             }
-            
+            .padding(.horizontal)
+
         }
         
         
